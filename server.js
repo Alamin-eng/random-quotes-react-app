dcc1ec156f41dd2208a57eaa0db8e2642e1c94bb
Alamin-eng <alaminruben@gmail.com>
2023-02-19 01:22:49 +0000

    final

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+let quotes = require("./quotes.json");
+app.get("/", async function (req, res) {
+  let x = Math.floor(Math.random() * quotes.length);
+  await res.send(quotes[x]);
+});
+
+
+app.listen(3002, function () {
+  console.log("Server is listening on port 3002. Ready to accept requests!");
+});
